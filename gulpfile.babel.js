7e7c130e7e6882755ef4e1a143ccbb0bb4234c9b
Arnelle Balane <arnellebalane@gmail.com>
2016-02-07 22:38:34 +0800

    ghk-lint-js - initial project setup

---
+++ gulpfile.babel.js
@@ -0,0 +1,27 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import babel from 'gulp-babel';
+
+
+const paths = {
+    javascripts: ['source/**/*.js']
+};
+
+
+gulp.task('javascripts', () => {
+    return gulp.src(paths.javascripts, { base: 'source' })
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest('distribution'));
+});
+
+
+gulp.task('build', ['javascripts']);
+
+
+gulp.task('watch', () => {
+    gulp.watch(paths.javascripts, ['javascripts']);
+});
+
+
+gulp.task('default', ['build', 'watch']);
---
+++ readme.md
@@ -0,0 +1,13 @@
+ghk-lint-js
+===================
+
+git hook to lint js files before committing them
+
+plugin for **[ghk](https://www.npmjs.com/package/ghk)** package
+
+
+#### installation
+
+```
+$ npm install --save ghk-lint-js
+```
